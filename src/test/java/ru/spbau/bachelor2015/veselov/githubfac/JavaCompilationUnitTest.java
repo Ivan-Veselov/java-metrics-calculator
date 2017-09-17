a9ffdb6279bd67bf7922bdd22a2fceecdbf4b6e0
Ivan Veselov <veselov.iva@gmail.com>
2017-09-18 00:28:11 +0300

    Create TestEnvironment class.

--- src/test/java/ru/spbau/bachelor2015/veselov/githubfac/JavaCompilationUnitTest.java
+++ src/test/java/ru/spbau/bachelor2015/veselov/githubfac/JavaCompilationUnitTest.java
@@ -1,33 +1,20 @@
 package ru.spbau.bachelor2015.veselov.githubfac;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import javaslang.collection.List;
-import org.apache.commons.io.FileUtils;
-import org.jetbrains.annotations.NotNull;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
-public class JavaCompilationUnitTest {
-    @Rule
-    public TemporaryFolder projectFolder = new TemporaryFolder();
-
+public class JavaCompilationUnitTest extends TestEnvironment {
     @Test
     public void getMethodsDeclarationsTest() throws Exception {
         File file = addSourceFileToProjectDir("/Methods.java");
         JavaParserTypeSolver typeSolver = getNewTypeSolver();
-        JavaCompilationUnit compilationUnit = getCompilationUnit(file, typeSolver);
+        JavaCompilationUnit compilationUnit = getJavaCompilationUnit(file, typeSolver);
 
         /*
             TODO: Invalid test.
@@ -45,24 +32,5 @@ public class JavaCompilationUnitTest {
                                              "Methods.method4",
                                              "Methods.InnerClass.method5",
                                              "HiddenClass.method6"));
-    }
-
-    private @NotNull File addSourceFileToProjectDir(final @NotNull String fileName)
-                                                                                throws Exception {
-        Path pathToFile = Paths.get(getClass().getResource(fileName).getFile());
-
-        File newFile = projectFolder.newFile(pathToFile.getFileName().toString());
-        FileUtils.copyFile(pathToFile.toFile(), newFile);
-
-        return newFile;
-    }
-
-    private @NotNull JavaParserTypeSolver getNewTypeSolver() {
-        return new JavaParserTypeSolver(projectFolder.getRoot());
-    }
-
-    private @NotNull JavaCompilationUnit getCompilationUnit(final @NotNull File file,
-                                final @NotNull JavaParserTypeSolver typeSolver) throws Exception {
-        return new JavaCompilationUnit(JavaParser.parse(file), typeSolver);
     }
 }
---
+++ src/test/java/ru/spbau/bachelor2015/veselov/githubfac/TestEnvironment.java
@@ -0,0 +1,36 @@
+package ru.spbau.bachelor2015.veselov.githubfac;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestEnvironment {
+    @Rule
+    public TemporaryFolder projectFolder = new TemporaryFolder();
+
+    protected @NotNull File addSourceFileToProjectDir(final @NotNull String fileName)
+                                                                                throws Exception {
+        Path pathToFile = Paths.get(getClass().getResource(fileName).getFile());
+
+        File newFile = projectFolder.newFile(pathToFile.getFileName().toString());
+        FileUtils.copyFile(pathToFile.toFile(), newFile);
+
+        return newFile;
+    }
+
+    protected @NotNull JavaParserTypeSolver getNewTypeSolver() {
+        return new JavaParserTypeSolver(projectFolder.getRoot());
+    }
+
+    protected @NotNull JavaCompilationUnit getJavaCompilationUnit(final @NotNull File file,
+                                  final @NotNull JavaParserTypeSolver typeSolver) throws Exception {
+        return new JavaCompilationUnit(JavaParser.parse(file), typeSolver);
+    }
+}
