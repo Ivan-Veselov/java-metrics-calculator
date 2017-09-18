fd6c98773a4f6399f3a8fb50ac9e8520de0f487d
Ivan Veselov <veselov.iva@gmail.com>
2017-09-18 11:48:39 +0300

    Implement getTextualAST method.

--- src/main/java/ru/spbau/bachelor2015/veselov/githubfac/JavaMethodDeclaration.java
+++ src/main/java/ru/spbau/bachelor2015/veselov/githubfac/JavaMethodDeclaration.java
@@ -1,8 +1,10 @@
 package ru.spbau.bachelor2015.veselov.githubfac;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class JavaMethodDeclaration {
@@ -37,6 +39,37 @@ public class JavaMethodDeclaration {
     }
 
     public @NotNull String getTextualAST() {
-        throw new UnsupportedOperationException();
+        class TextBuilder {
+            private static final int INDENT_INC_SIZE = 2;
+
+            private final @NotNull StringBuilder stringBuilder = new StringBuilder();
+
+            public TextBuilder(final @NotNull Node node) {
+                traverse(node, 0);
+            }
+
+            public @NotNull String getText() {
+                return stringBuilder.toString();
+            }
+
+            private void traverse(final @NotNull Node node, final int indent) {
+                stringBuilder.append(StringUtils.repeat(' ', indent))
+                             .append(node.getClass().getSimpleName());
+
+                if (node.getChildNodes().isEmpty()) {
+                    stringBuilder.append('(')
+                                 .append(node)
+                                 .append(')');
+                }
+
+                stringBuilder.append('\n');
+
+                for (Node child : node.getChildNodes()) {
+                    traverse(child, indent + INDENT_INC_SIZE);
+                }
+            }
+        }
+
+        return new TextBuilder(methodDeclaration).getText();
     }
 }
